3c457c3b95db6b6da646b84aeb9be4c6a2dac5f6
illya <1367693858@QQ.COM>
2019-04-15 13:57:06 +0800

    Add files via upload

--- Python3.6/mnist/detimg.py
+++ Python3.6/mnist/detimg.py
@@ -10,24 +10,35 @@ import matplotlib.image as mpimg
 from PIL import Image
 import numpy as np
 import pytorch_cnn as cnn
+import svdnn
+
+def sig(x):
+    x=x*10
+    y = 1.0 / (1.0 + np.exp(-x))
+    y=np.uint8 (y*255)
+    return y
+
 def possesser(a):
-    #a=a.tobytes()
-    a=np.reshape(a,(28*28))
-    for i,v in enumerate(a):
-        if v<128:
-            a[i]=0x00
-        else :
-            a[i]=0xff
-    return np.reshape(a,(28,28))
-lena=Image.open('0.png')
-lena=lena.resize((28,28),Image.ANTIALIAS)
-lena=lena.convert('L')
-lena = np.array(lena)
+    x,y=a.size
+    a=a.convert('L')
+    a=np.array(a)
+    mid = np.mean(a)
+    a=a-mid
+    a=sig(a)
+    a=Image.fromarray(a).convert('L')
+    a=a.resize((28,28),Image.ANTIALIAS)
+    return a
+
+lena=Image.open('2.png')
+
 lena=possesser(lena)
-#lena = np.array(lena)
+
+lena=np.reshape(lena,[28,28])
+
 print (lena)
 plt.imshow(lena,cmap="gray") 
 plt.axis('on') 
 plt.show()
-out=cnn.detect(lena)
-print (out)
+out1=svdnn.detect(lena)
+out2=cnn.detect(lena)
+print ("svd =",out1,"cnn =",out2)
