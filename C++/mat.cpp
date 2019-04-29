b32733b7a3fd77d38d74f518874cf8f2970e3bb0
Wendell King <1367693858@qq.com>
2019-04-29 16:06:41 +0800

    change

--- C++/mat.cpp
+++ C++/mat.cpp
@@ -3,17 +3,14 @@ using namespace std;
 class mat{
     private:
 
-        double ** data=nullptr;
+        double * data=nullptr;
         int row=0;
         int column=0;
 
         void free(){
             if(this->data==nullptr)return;
             else{
-                for (int i=0;i<this->row;i++){
-                delete this->data[i];
-            }
-            delete this->data;
+                delete this->data;
             }
         }
     public :
@@ -22,19 +19,19 @@ class mat{
 
             this ->row=row;
             this ->column=column;
-            this->free();
-            this ->data= new double *[this->row];
+            this ->free();
+            this ->data= new double [this->row*this->column];
 
             for (int i=0;i<this->row;i++){
-                this ->data[i]=new double [this->column];
-                if(tdata!=nullptr)
-
+                if(tdata!=nullptr){
                     for(int j=0;j<this->column;j++){
-                        this->data[i][j]=tdata[this->column*i+j];
-
+                        this->data[i*this->column+j]=tdata[this->column*i+j];
                     }
+                }
 
             }
+
+
 
         }
 
@@ -42,13 +39,12 @@ class mat{
 
             this ->row=tmat.row;
             this ->column=tmat.column;
-            this->free();
-            this ->data= new double *[this->row];
+            this ->free();
+            this ->data= new double [this->row*this->column];
 
             for (int i=0;i<this->row;i++){
-                this ->data[i]=new double [this->column];
                 for(int j=0;j<this->column;j++){
-                    this->data[i][j]=tmat.data[i][j];
+                    this->data[i*this->column + j]=tmat.data[i*this->column + j];
                 }
             }
         }
@@ -58,29 +54,24 @@ class mat{
             this ->row=ptmat->row;
             this ->column=ptmat->column;
             this->free();
-            this ->data= new double *[this->row];
+            this ->data= new double [this->row*this->column];
 
             for (int i=0;i<this->row;i++){
-                this ->data[i]=new double [this->column];
                 for(int j=0;j<this->column;j++){
-                    this->data[i][j]=ptmat->data[i][j];
+                    this->data[i*this->column + j]=ptmat->data[i*this->column + j];
                 }
             }
         }
 
         ~mat(){
-            for (int i=0;i<this->row;i++){
-                delete this->data[i];
-            }
             delete this->data;
-
         }
 
         void show(){
 
             for (int i=0;i<this->row;i++){
                 for (int j=0;j<this->column;j++){
-                    cout<<this->data[i][j]<<' ';
+                    cout<<this->data[i*this->column + j]<<' ';
                 }
 
                 cout<<endl;
@@ -101,23 +92,24 @@ class mat{
             return temmat;
         }
 
-        mat* operator+(const mat* m){
+        mat* add(const mat* m){
+            new mat(this->row,this->column);
             mat *temmat= new mat(this->row,this->column);
+
             if(this->row!=m->row||this->column!=m->column){
                 cout<<"can not plus"<<endl;
                 return  temmat;
             }
-
             for(int i=0;i<this->row;i++){
                 for(int j=0;j<this->column;j++){
-                    temmat->data[i][j]=this->data[i][j]+m->data[i][j];
+                    temmat->data[i*this->column + j]=this->data[i*this->column + j]+m->data[i*this->column + j];
                 }
             }
-
             return temmat;
         }
 
-        mat* operator-(const mat* m){
+        mat* sub (const mat* m){
+
             mat *temmat=new mat(this->row,this->column);
             if(this->row!=m->row||this->column!=m->column){
                 cout<<"can not sub"<<endl;
@@ -126,15 +118,17 @@ class mat{
 
             for(int i=0;i<this->row;i++){
                 for(int j=0;j<this->column;j++){
-                    temmat->data[i][j]=this->data[i][j]-m->data[i][j];
+                    temmat->data[i*this->column + j]=this->data[i*this->column + j]-m->data[i*this->column + j];
                 }
             }
 
             return temmat;
         }
-        mat* operator*(const mat* m){
+
+        mat* dot(const mat* m){
 
             mat * temmat= new mat(this->row,m->column);
+
             if(this->column!=m->row){
                 cout<<"can not multiple"<<endl;
                 return temmat;
@@ -145,34 +139,34 @@ class mat{
 
                     double temij=0;
                     for(int k=0;k<this->column;k++){
-                        temij=temij+this->data[i][k]*m->data[k][j];
+                        temij=temij+this->data[i*this->column + k]*m->data[k*m->column + j];
                     }
-                    temmat->data[i][j]=temij;
+                    temmat->data[i*m->column + j]=temij;
                 }
             }
-
             return temmat;
         }
 
-        mat* operator*(const double & m){
+        mat* dot(const double & m){
 
             mat *temmat= new mat(this->row,this->column);
 
             for(int i=0;i<this->row;i++){
                 for(int j=0;j<this->column;j++){
-                    temmat->data[i][j]=this->data[i][j]*m;
+                    temmat->data[i*this->column + j]=this->data[i*this->column + j]*m;
                 }
             }
 
             return temmat;
         }
-        mat* operator/(const double& m){
+
+        mat* exc (const double& m){
 
             mat * temmat= new mat(this->row,this->column);
 
             for(int i=0;i<this->row;i++){
                 for(int j=0;j<this->column;j++){
-                    temmat->data[i][j]=this->data[i][j]/m;
+                    temmat->data[i*this->column + j]=this->data[i*this->column + j]/m;
                 }
             }
 
@@ -180,55 +174,36 @@ class mat{
         }
 
         mat *trans(){
-            mat *temmat= new mat(this->column,this->row);
 
+            mat *temmat= new mat(this->column,this->row);
             for(int i=0;i<this->row;i++){
                 for(int j=0;j<this->column;j++){
-                    temmat->data[j][i]=this->data[i][j];
+                    temmat->data[j*this->row + i]=this->data[i*this->column + j];
                 }
             }
 
             return temmat;
         }
 
-        void setdata(int row=0,int column=0, double * tdata=nullptr){
-
-            for (int i=0;i<this->row;i++){
-                delete this->data[i];
-            }
-            delete this->data;
-
-            this ->row=row;
-            this ->column=column;
-            this ->data= new double *[this->row];
-
-            for (int i=0;i<this->row;i++){
-                this ->data[i]=new double [this->column];
-                if(tdata!=nullptr)
-                    for(int j=0;j<this->column;j++){
-                        this->data[i][j]=tdata[this->column*i+j];
-                    }
-
-            }
-
-        }
-        //注意释放内存 delete dous
         double * todoubles(){
-
-            double *dous = new double[this->column*this->row];
-            for(int i=0;i<this->row;i++){
-                for(int j=0;j<this->column;j++){
-                    dous[i*this->column+j]=this->data[i][j];
-                }
-            }
-
-            return dous;
+            return this->data;
         }
+
         int getrow(){
             return this->row;
         }
+
         int getcolumn(){
             return this->column;
+        }
+
+        mat inverse(){
+            mat temmat= mat::eye(this->column);
+            if(this->column!=this->row){
+                cout<<"can not inverse"<<endl;
+                return temmat;
+            }
+
         }
 
 
